1e906142d5f2e863820cf48bfa32421a3c01d286
Gordon-928 <94927083+Gordon-928@users.noreply.github.com>
2022-11-14 00:30:15 -0800

    Homing Function Testing - Stepper move as expected, can proceed to fully structure the committed - The following must be impletemented into the code to ensure code is up-to-speed     - run run() and runSpeed() as often as possible     - always run .setMaxSpeed() at setup(), or the stepper will rotate extermly slow as we are using high res micro stepping - Speed of each axis shall be set to proper value to ensure they have proper speed

---
+++ Filament-Winder/src/main.cpp
@@ -0,0 +1,100 @@
+#include <Arduino.h>
+#include "AccelStepper.h"
+
+#define Stp_EN 22
+
+#define StpX_S 4
+#define StpX_D 5
+#define StpX_HomeLim 18 //INT pins (18,19,20,21 on ArduinoMega) are used for optimization  
+#define
+
+#define StpZ_S 6 
+#define StpZ_D 7
+#define StpZ_Lim 19
+
+
+#define StpC_S 8
+#define StpC_D 9
+#define StpC_Lim 20
+
+//#define StpB_Stp 10 //Not used currently
+//#define StpB_Dir 11 //Not used currently
+//#define StpB_Lim 21
+
+AccelStepper StpX(AccelStepper::DRIVER, StpX_S, StpX_D);
+AccelStepper StpZ(AccelStepper::DRIVER, StpZ_S, StpZ_D);
+AccelStepper StpC(AccelStepper::DRIVER, StpC_S, StpC_D);
+//AccelStepper StpB(AccelStepper::DRIVER, StpB_S, StpB_D);
+
+inline void homing(int limitSW, AccelStepper Stepper){
+  Stepper.setSpeed(5000);
+    
+  while(digitalRead(18) == HIGH){ //while the limit SW is not touched
+    Stepper.runSpeed();
+    //Stepper.run();
+  }
+  Stepper.setSpeed(0);
+  Stepper.setCurrentPosition(0);
+}
+
+
+void setup() {
+
+  pinMode(StpX_Lim, INPUT_PULLUP);
+  pinMode(StpZ_Lim, INPUT_PULLUP);
+  pinMode(StpC_Lim, INPUT_PULLUP);
+  //pinMode(StpB_Lim, INPUT_PULLUP);
+  
+  Serial.begin(115200);
+  while(!Serial); //hold the device in loop until Serial is started
+  Serial.println("Init");
+
+  StpX.setEnablePin(Stp_EN);
+  StpX.setSpeed(140000);
+  StpX.setAcceleration(14000);
+
+  StpZ.setEnablePin(Stp_EN);
+  StpZ.setSpeed(140000);
+  StpZ.setAcceleration(14000);
+
+  StpC.setEnablePin(Stp_EN);
+  StpC.setMaxSpeed(300000);
+  StpC.setAcceleration(500000);   
+
+  //disable all the output to let user to position freely
+  StpX.disableOutputs();
+  StpZ.disableOutputs();
+  StpC.disableOutputs();
+  Serial.println("Setup complete, Position the axis away from home. Press any button when finished");
+  while (Serial.available() && Serial.read()); // empty buffer
+  while (!Serial.available());                 // wait for data
+  while (Serial.available() && Serial.read()); // empty buffer again
+
+  //enable all steppers
+  StpX.enableOutputs();
+  StpZ.enableOutputs();
+  StpC.enableOutputs();
+
+  Serial.println("Stepper enabled, press any button to start Homing");
+  while (Serial.available() && Serial.read()); // empty buffer
+  while (!Serial.available());                 // wait for data
+  while (Serial.available() && Serial.read()); // empty buffer again
+
+  Serial.println("Current Action: Homing X then Z");
+  
+  //Home X
+  digitalWrite(Stp_EN, LOW);
+  homing(StpX_Lim,StpC);
+  //StpC.setSpeed(5000);
+  //Home Z
+}
+
+void loop() {
+  //Home X
+  //StpC.runSpeed();
+  //homing(StpX_Lim,StpX);
+
+
+  //Home Z
+
+}
