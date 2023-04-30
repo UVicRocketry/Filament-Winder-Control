b3a651a424eb2cb0a96dfd21b43798e4bd822246
Gordon-928 <94927083+Gordon-928@users.noreply.github.com>
2023-04-30 16:39:31 -0700

    Added Pinouts file

--- grbl-Mega-5X-edge/grbl/config.h
+++ grbl-Mega-5X-edge/grbl/config.h
@@ -52,7 +52,7 @@
 // unpredictable behavior!
 //----------------------------------------------------------------------
 
-#define N_AXIS 5        // Number of axes (3 to 6)
+#define N_AXIS 6        // Number of axes (3 to 6)
 #define N_AXIS_LINEAR 3 // Number of linears axis, must be <= N_AXIS
 
 // Axis indexing and names
